39068a769e01eb5a240b1ef7a95b5f10a66c6013
maimaiyeuem12009 <68004995+maimaiyeuem12009@users.noreply.github.com>
2021-10-09 11:25:51 +0700

    add windicss

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
+import WindiCSS from 'vite-plugin-windicss'
 import vitePluginString from 'vite-plugin-string'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader(),vitePluginString()],
+  plugins: [vue(), svgLoader(),vitePluginString(),WindiCSS()],
   css: {
     preprocessorOptions: {
       scss: {
